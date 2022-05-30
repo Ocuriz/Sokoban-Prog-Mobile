3cf4fd1d43a6d4184189dc5f20e43b52daf7c9ad
jathanase <athanaseje@gmail.com>
2022-05-30 12:16:03 +0200

    Database config

--- app/src/main/java/com/example/sokoban/Gametable.java
+++ app/src/main/java/com/example/sokoban/Gametable.java
@@ -2,13 +2,75 @@ package com.example.sokoban;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Gametable extends AppCompatActivity {
+
+    public void initGame(char[][] plateau){
+        char[] ligne;
+        Case[][] plateauFinal = new Case[plateau.length][];
+        for(int i = 0; i < plateau.length; i++){
+            ligne = plateau[i];
+            Case[] finalLigne = new Case[ligne.length];
+            for(int j = 0; j < ligne.length; j++){
+                Case element = new Case(i,j);
+                element.setType(String.valueOf(ligne[j]));
+                finalLigne[j] = element;
+            }
+            plateauFinal[i] = finalLigne;
+        }
+        Log.i("test", String.valueOf(plateauFinal[2][1].getType()));
+    }
+    /*goLeft(){
+        if("à compléter" > 0 && .type != CaseType.MUR){
+            player.setX(player.getX() - 1);
+        }
+    }*/
+
+    /*goRight(){
+        if("à compléter" < .numColumns && .type != CaseType.MUR){
+            player.setX(player.getX() + 1);
+        }
+    }*/
+
+    /*goTop(){
+        if("à compléter" > 0 && .type != CaseType.MUR){
+            player.setY(player.getY() - 1);
+        }
+    }*/
+
+    /*goBack(){
+        if("à compléter" < .numColumns && .type != CaseType.MUR){
+            player.setY(player.getY() - 1);
+        }
+    }*/
+
+    /*
+        ..####...
+        ###..####
+        #.....C.#
+        #.#..#C.#
+        #.x.x#P.#
+        #########
+    */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gametable);
+        setContentView(R.layout.activity_main);
+        DataBaseHelper db = new DataBaseHelper();
+        char[][] temp = {
+                {'.', '.', '#', '#', '#', '#', '.', '.', '.'},
+                {'#', '#', '#','.', '.','#', '#', '#','#'},
+                {'#', '.', '.', '.', '.', '.', 'C', '.', '#'},
+                {'#', '.', '#', '.', '.', '#', 'C', '.', '#'},
+                {'#', '.', 'X', '.', 'X', '#', 'P', '.', '#'},
+                {'#', '#', '#', '#', '#', '#', '#', '#', '#'}
+        };
+
     }
 }
